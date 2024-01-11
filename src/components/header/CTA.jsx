05319b4e93573a7208c0471392ddc8497edc3f89
KaranAhir2552 <106022800+KaranAhir2552@users.noreply.github.com>
2024-01-11 12:04:02 +0530

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import CV from '../../assets/Karan_resume.pdf';
+//import CV from '../../assets/Karan_resume.pdf';
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
-        Download CV
+      <a href="#" download className="btn">
+        Download CV (currently There is not updated CV)
       </a>
       <a href="#contact" className="btn btn-primary">
         Let's talk
