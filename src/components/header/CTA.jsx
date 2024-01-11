82cf517796644696f0808166c779fd59967b2607
KaranAhir2552 <106022800+KaranAhir2552@users.noreply.github.com>
2024-01-11 12:04:46 +0530

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
   return (
     <div className="cta">
       <a href="#" download className="btn">
-        Download CV (currently There is not updated CV)
+        Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
         Let's talk
